0cfa8b30fadc3338161139823089963ef207560c
Thomas Sclater <44801711+tomsclater@users.noreply.github.com>
2024-12-23 02:18:46 +0000

    UPdate

--- pages/project1.js
+++ pages/project1.js
@@ -2,7 +2,7 @@ import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
 import Project1 from "../components/Project1";
 
-export default function contact() {
+export default function project1() {
   return (
     <ContainerBlock>
       <Project1 />
