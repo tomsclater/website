d9b72e8a56483483834e16cc4177d1b7e78ef127
Thomas Sclater <44801711+tomsclater@users.noreply.github.com>
2024-12-23 05:44:58 +0000

    Update

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -7,7 +7,7 @@ export default function FavouriteProjects() {
       <div className="max-w-6xl mx-auto">
         <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
           <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center">
-            Favourite Projects
+            Project Highlights
           </h1>
           <Link
             href="/projects"
